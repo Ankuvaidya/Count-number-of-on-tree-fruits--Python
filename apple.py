45f08be8191cac61570c06fbb56f2f49aaee5501
Ankita Vaidya <31740151+Ankuvaidya@users.noreply.github.com>
2018-04-30 21:47:26 +0530

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+lowerBound=np.array([170,100,80])
+upperBound=np.array([180,256,256])
+
+#cam= cv2.VideoCapture(0)
+kernelOpen=np.ones((5,5))
+kernelClose=np.ones((20,20))
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+while True:
+    img=cv2.imread('apple.jpg')
+    #img=cv2.resize(img,(340,220))
+
+    #convert BGR to HSV
+    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    cv2.imshow('HSV',imgHSV)
+    # create the Mask
+    mask=cv2.inRange(imgHSV,lowerBound,upperBound)
+    #morphology
+    maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+    maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+
+    maskFinal=maskClose
+    _,conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    
+    cv2.drawContours(img,conts,-1,(255,0,0),1)
+    for i in range(len(conts)):
+        x,y,w,h=cv2.boundingRect(conts[i])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255), 2)
+        cv2.putText(img, str(i+1),(x,y+h),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0))
+        
+    cv2.imshow("maskClose",maskClose)
+    cv2.imshow("maskOpen",maskOpen)
+    cv2.imshow("mask",mask)
+    cv2.imshow("cam",img)
+    if cv2.waitKey(10) &0xFF ==ord('q'):
+                cap.release()
+                cv2.destroyAllWindows()
+                break
